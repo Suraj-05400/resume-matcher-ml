6325210412a9b6c504526623e5dcf51e8623189d
Yuvaraja1717 <yuvarajrsyuvi01@gmail.com>
2026-04-14 13:08:31 +0530

    Add logistic regression functions to model.py

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.linear_model import LogisticRegression
 
 
+# ---------- TEXT SIMILARITY ----------
 def match_resumes(resumes, job_description):
 
     documents = [job_description] + resumes
@@ -14,3 +16,17 @@ def match_resumes(resumes, job_description):
     scores = [float(score[0]) for score in similarity]
 
     return scores
+
+
+# ---------- LOGISTIC REGRESSION ----------
+def train_logistic_model(features, labels):
+
+    model = LogisticRegression()
+    model.fit(features, labels)
+
+    return model
+
+
+def predict_logistic(model, features):
+
+    return model.predict(features)
